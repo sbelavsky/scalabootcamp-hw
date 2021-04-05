49763afba07f72cedc1980e0f508c4be0f799624
Sergey Belavsky <sergey.belavsky@idt.net>
2021-04-05 23:27:37 +0300

    feat: hw12

---
+++ src/main/scala/com/evolution/bootcamp/async/AsyncHomework.scala
@@ -0,0 +1,57 @@
+package com.evolution.bootcamp.async
+
+import java.net.URL
+import java.util.concurrent.Executors
+
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.io.Source
+
+/**
+ * Application:
+ * - takes a web-page URL from arguments (args array)
+ * - loads the web-page body, extracts HTTP links from it
+ * - for all the found links, tries to fetch a server name header if there is one
+ * - prints all the encountered unique server name values in alphabetical order
+ *
+ * Each link processing should be done in parallel.
+ * Validation of arguments is not needed.
+ *
+ * Try to test it on http://google.com!
+ */
+object AsyncHomework extends App {
+  private implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
+
+  val names = for {
+    body <- fetchPageBody(args(0))
+    links <- findLinkUrls(body)
+    serverNames <- Future.sequence(links.map(fetchServerName))
+  } yield serverNames.filterNot(_.isEmpty).map(_.get).distinct.sorted
+
+  println(Await.result(names, 30.seconds))
+
+  private def fetchPageBody(url: String): Future[String] = {
+    println(f"Fetching $url")
+    Future {
+      val source = Source.fromURL(url)
+      try {
+        source.mkString
+      } finally {
+        source.close()
+      }
+    }
+  }
+
+  private def fetchServerName(url: String): Future[Option[String]] = {
+    println(s"Fetching server name header for $url")
+    Future {
+      Option(new URL(url).openConnection().getHeaderField("Server"))
+    }
+  }
+
+
+  private def findLinkUrls(html: String): Future[List[String]] = Future {
+    val linkPattern = """href="(http[^"]+)"""".r
+    linkPattern.findAllMatchIn(html).map(m => m.group(1)).toList
+  }
+}
